251d599502dc950fbe86f047fc40e2694735fc94
DR <ramdr876@gmail.com>
2023-08-03 19:05:56 +0530

    updated

--- Day1.py
+++ Day1.py
@@ -18,3 +18,36 @@
 # print(a.title())
 # print(f"input()[:3].title()  input()[-3:] tenator") # first 3 characters of fname,lastname and add tenator
 #upper swapcase title
+
+
+#replace name 
+# name = input() 
+# for i in name: 
+#     print(f"{i*int(name.find(i)+1)}") 
+#     name = name.replace(i,'*',1)
+
+# #Sample code to print number greater than 5
+# num=input()
+# r=5
+# if int(num)>r:
+#     while(int(num)>r):
+#         print(r+1)
+#         r=r+1
+# else:
+#     print("Only numbers greater than {r} are allowed")
+import math
+list = []
+factorial = []
+listcount = int(input("Enter list count"))
+for i in range(listcount):
+    list.append(int(input("Enter number")))
+    factorial.append(factorial.list[i])
+
+print(factorial)
+
+
+
+
+
+
+
--- Practice1.py
+++ Practice1.py
@@ -1,7 +1,47 @@
 
-def reverseAString():
-    print("Hello!")
+#Function to reverse a string using slicing
+def reverseAString(string):
+    return string[::-1]
+
+# Alternate Function to reverse a string
+def reverseUsingJoin(string):
+    string = [string[i] for i in range(len(string)-1, -1, -1)]
+    return "".join(string)
+
+#Function to reverse each word in a sentence
+def reverseWordsSentence(S):
+    words = S.split(" ") #split sentence by space
+    ans = ""
+    for i in words:
+        ans = i[::-1] + " " + ans   
+    return ans[:len(ans)-1] # truncate space in the end of sentence
+
+#Function to reverse string seperated by .(not charcters but by word)
+def reverseWords(S):
+    words = S.split(".") #split sentence by space
+    ans = ""
+    for i in range(len(words)-1,-1,-1):
+        ans = ans + "." + words[i]
+        print(ans)
+    return ans[1:]# truncate . in the start of sentence  
+
+#Check if string is anagram
+def isAnagram(a,b):
+        a = ''.join(sorted(a))
+        b = ''.join(sorted(b))
+        if a == b:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-    reverseAString()
-
+    # revstring = reverseAString("Deepthi")
+    # print(f"Reversed String = {revstring}")
+    # revstring = reverseUsingJoin("Deepthi")
+    # print(f"Reversed String = {revstring}")
+    # reversewordsinsentence = reverseWordsSentence("How are you?")
+    # print(f"Reversed words in given sentence : {reversewordsinsentence}")
+    reverseWordsResult = reverseWords("i.like.you")
+    print(f"Reversed words in given sentence : {reverseWordsResult}")
+    anagramCheck = isAnagram("swattl","swaltt")
+    print(f"Result for Anagram check : {anagramCheck}")
